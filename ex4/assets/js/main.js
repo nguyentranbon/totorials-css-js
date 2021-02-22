88bc2f445a4aca44e1b9ea0cd7381d0e6f382889
nguyentb <nguyentb1999vn@gmail.com>
2021-02-23 00:34:03 +0700

    update 50% "web-restaurant"

---
+++ ex4/assets/js/main.js
@@ -0,0 +1,12 @@
+/* SHOW MENU */
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('nav__menu-show')
+        });
+    }
+}
+showMenu('nav-toggle','nav-menu');
+console.log("12312")
